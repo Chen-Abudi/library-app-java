55f90c05cc11c636a891054e3d1b99ff28f496ad
Grace Chen Abudi <cheezumcali23@gmail.com>
2024-06-23 20:01:00 +0300

    feat: Check books in and out of the library

--- src/libapp/LibraryApp.java
+++ src/libapp/LibraryApp.java
@@ -14,7 +14,7 @@ public class LibraryApp {
                     book.getTitle(), book.getGenre(), book.getAuthor());
         }
         else {
-            System.out.printf("0 books found.");
+            System.out.println("0 books found.");
         }
 
         System.out.print("\n\n");
@@ -29,6 +29,38 @@ public class LibraryApp {
         for (Book book : books) {
             System.out.printf("\tTitle: %s\n\tGenre: %s\n\tAuthor: %s\n\t---\n",
                     book.getTitle(), book.getGenre(), book.getAuthor());
+        }
+        System.out.println();
+    }
+
+    public void checkOutBook(String isbn) {
+        Book book = bookRepo.findByIsbn(isbn);
+
+        if (book != null) {
+            book.checkOut();
+            System.out.println("Book checked out successfully:");
+            System.out.printf("\tISBN: %s\n\tTitle: %s\n\tAuthor: %s\n",
+                    book.getIsbn(), book.getTitle(), book.getAuthor());
+        }
+        else {
+            System.out.println("Failed to check out book.\n");
+            System.out.printf("Reason: There is no book with ISBN %s on record.\n", isbn);
+        }
+        System.out.println();
+    }
+
+    public void checkInBook(String isbn) {
+        Book book = bookRepo.findByIsbn(isbn);
+
+        if (book != null) {
+            book.checkOut();
+            System.out.println("Book checked in successfully:");
+            System.out.printf("\tISBN: %s\n\tTitle: %s\n\tAuthor: %s\n",
+                    book.getIsbn(), book.getTitle(), book.getAuthor());
+        }
+        else {
+            System.out.println("Failed to check out book.\n");
+            System.out.printf("Reason: There is no book with ISBN %s on record.\n", isbn);
         }
         System.out.println();
     }
--- src/libapp/Main.java
+++ src/libapp/Main.java
@@ -3,8 +3,9 @@ package libapp;
 public class Main {
     public static void main(String[] args) {
         LibraryApp app = new LibraryApp();
-        app.searchByTitle("castle");
-        app.searchByTitle("night");
-        app.searchByTitle("dagger");
+        app.checkOutBook("06917");
+        app.checkOutBook("72391");
+
+        app.checkInBook("39662");
     }
 }
